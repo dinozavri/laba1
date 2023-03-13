12661588fd96f642bcc36ed5cf076a1f9668b181
Yasmina <kudinayasmina15@gmail.com>
2023-03-13 16:31:47 +0300

    Third Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country= new Country();
+            country.Name = "Belarus";
+            country.Form_of_government = "Республика";
+            country.Area = 208000;
+        }
+    }
+}
