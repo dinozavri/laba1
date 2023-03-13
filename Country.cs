df6e5aa722096d691afe0b634caafc8554671eca
Yasmina <kudinayasmina15@gmail.com>
2023-03-13 16:12:52 +0300

    First Commit

---
+++ Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr_1
+{
+    class Country
+    {
+        public string Name{get; set;}
+        public string Form_of_government{ get; set;}
+        public float Area { get; set;}
+    }
+}
